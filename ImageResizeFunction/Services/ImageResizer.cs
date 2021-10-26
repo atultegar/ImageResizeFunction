fa5ce6fd105b57c62dbe0631d8b4937c61fd642c
Atul Tegar <atul.tegar@gmail.com>
2021-10-26 14:44:45 +0200

    Add project files.

---
+++ ImageResizeFunction/Services/ImageResizer.cs
@@ -0,0 +1,22 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ImageResizeFunction.Services
+{
+    public class ImageResizer : IImageResizer
+    {
+        public void Resize(Stream input, Stream output)
+        {
+            using (Image image = Image.Load(input))
+            {
+                image.Mutate(x => x.Resize(400, 0));
+                image.Save(output, new JpegEncoder());
+            }
+        }
+    }
+}
---
+++ ImageResizeFunction/Startup.cs
@@ -0,0 +1,18 @@
+﻿using ImageResizeFunction.Services;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(ImageResizeFunction.Startup))]
+namespace ImageResizeFunction
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<IImageResizer, ImageResizer>();
+        }
+    }
+}
